e84320a31ed8e027d80120ceefff305538053914
anin27 <aninthomas27@gmail.com>
2026-04-12 03:09:07 +0100

    fill the training event dropdown using events saved from the training events page

--- static/js/registrations.js
+++ static/js/registrations.js
@@ -1,35 +1,24 @@
-let registrations = JSON.parse(localStorage.getItem("registrations")) || [];
+let registrations =[]
 
+let savedRegistrations = localStorage.getItem("registrations");
+if (savedRegistrations) {
+    registrations = JSON.parsse(savedRegistrations);
+    
+}
 loadTrainingEvents();
-showRegistrations();
+showRegistration();
 
 function loadTrainingEvents() {
-    let events = JSON.parse(localStorage.getItem("events")) || [];
-    let trainingEventDropdown = document.getElementById("trainingEvent");
+    let dropdown = document.getElementById("trainingEvent");
+    dropdown.innerHTML = `<option value=""> Select Training Events</option>`;
 
-    trainingEventDropdown.innerHTML = '<option value="">Select Training Event</option>';
-
-    for (let i = 0; i < events.length; i++) {
-        trainingEventDropdown.innerHTML += `
-            <option value="${events[i].title}">${events[i].title}</option>
-        `;
+    let savedEvents = localStorage.getItem("events");
+    let events = [];
+    if (savedEvents){
+        events = JSON.parse(savedEvents);
+        for (let i = 0, i <events.length; i++){
+            let option = "<opion>" + events[1].title + "</options>";
+            dropdown.innerHTML += option;
+        }
     }
 }
-
-function saveRegistration() {
-    let registrationId = document.getElementById("registrationId").value;
-    let employeeName = document.getElementById("employeeName").value;
-    let employeeId = document.getElementById("employeeId").value;
-    let department = document.getElementById("department").value;
-    let trainingEvent = document.getElementById("trainingEvent").value;
-    let status = document.getElementById("status").value;
-
-    if (
-        employeeName === "" &&
-        employeeId === "" &&
-        department === "" &&
-        trainingEvent === "" &&
-        status === ""
-    ){
-        return;
-    }
